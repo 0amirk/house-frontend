f4de68ce2fe66d32e3904a52e5959082740afcc3
harshau007 <amanupadhyay2004@gmail.com>
2024-01-05 00:17:45 +0530

    Major Changes done on it pt 2

--- app/api/axios.js
+++ app/api/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+const [_document, set_document] = React.useState(null)
+
+React.useEffect(() => {
+    set_document(document)
+}, [])
+
+
 export default axios.create({
   baseURL: "https://house-backend-393e.onrender.com", // Prod 
   headers:{
-    'Authorization':'Bearer '+ document.cookie.replace(/(?:(?:^|.;\s)Authorization\s=\s([^;]).$)|^.*$/, '$1')
+    'Authorization':'Bearer '+ _document.cookie.replace(/(?:(?:^|.;\s)Authorization\s=\s([^;]).$)|^.*$/, '$1')
   }
 });
